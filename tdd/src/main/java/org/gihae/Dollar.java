2b84f34895497640507cae180f1c07b990720473
xcelxlorx <xcelxlorx@gmail.com>
2023-09-02 09:40:30 +0900

    feat: test1에서 5의 중복을 없애기
    - 5는 생성자에서 넘어오는 값이므로 amount에 저장되도록 하기

--- tdd/src/main/java/org/gihae/Dollar.java
+++ tdd/src/main/java/org/gihae/Dollar.java
@@ -3,9 +3,11 @@ package org.gihae;
 public class Dollar {
     public int amount;
 
-    public Dollar(int amount) {}
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
 
     public void times(int multiplier){
-        amount = 5 * 2;
+        amount = amount * 2;
     }
 }
