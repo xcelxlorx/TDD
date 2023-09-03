19d2cafc4a1d027535276f9d1af3a06da5c35ffb
xcelxlorx <xcelxlorx@gmail.com>
2023-09-04 07:53:23 +0900

    feat: 초기 test4
    - amount를 public하게 접근하고 있음 -> private으로 변경해야 함!

--- tdd/src/test/java/MultiplicationTest.java
+++ tdd/src/test/java/MultiplicationTest.java
@@ -26,4 +26,13 @@ class MultiplicationTest {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
+
+    @Test
+    void test4(){
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
 }
