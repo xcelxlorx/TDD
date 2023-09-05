c42c2a0d90f5bffd3587f3f80799c91f553905da
xcelxlorx <xcelxlorx@gmail.com>
2023-09-05 09:01:29 +0900

    feat: test6에서 Dollar의 equals()를 Money로 옮기기

--- tdd/src/main/java/org/gihae/Dollar.java
+++ tdd/src/main/java/org/gihae/Dollar.java
@@ -9,9 +9,4 @@ public class Dollar extends Money{
     public Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
     }
-
-    public boolean equals(Object obj) {
-        Money money = (Money) obj;
-        return amount == money.amount;
-    }
 }
--- tdd/src/main/java/org/gihae/Money.java
+++ tdd/src/main/java/org/gihae/Money.java
@@ -2,4 +2,9 @@ package org.gihae;
 
 public class Money {
     protected int amount;
+
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount;
+    }
 }
