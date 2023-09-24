2ca576ef57764208ba5f1999e2bc1704e06e177a
xcelxlorx <xcelxlorx@gmail.com>
2023-09-24 20:10:49 +0900

    feat: 초기 test9
    - 불필요한 하위 클래스를 제거하기 위해 통화 개념을 구현해야 함
    - 통화를 테스트하기 위해서 우선 문자열을 사용하자

--- tdd/src/test/java/MultiplicationTest.java
+++ tdd/src/test/java/MultiplicationTest.java
@@ -78,4 +78,11 @@ class MultiplicationTest {
         assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
+
+    @Test
+    void test9(){
+        //test currency
+        assertEquals("USD", Money.dollar(1).currency);
+        assertEquals("CHF", Money.franc(1).currency);
+    }
 }
