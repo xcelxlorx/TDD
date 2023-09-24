647f73da2bce34604ddfcaa30c447a3b2b9b8d16
xcelxlorx <xcelxlorx@gmail.com>
2023-09-24 20:20:02 +0900

    feat: 통화를 인스턴스 변수에 저장하고 생성자 만들기

--- tdd/src/main/java/org/gihae/Dollar.java
+++ tdd/src/main/java/org/gihae/Dollar.java
@@ -2,12 +2,15 @@ package org.gihae;
 
 public class Dollar extends Money{
 
+    private String currency;
+
     public Dollar(int amount) {
         this.amount = amount;
+        currency = "USD";
     }
 
     public String currency(){
-        return "USD";
+        return currency;
     }
 
     public Money times(int multiplier){
--- tdd/src/main/java/org/gihae/Franc.java
+++ tdd/src/main/java/org/gihae/Franc.java
@@ -2,12 +2,15 @@ package org.gihae;
 
 public class Franc extends Money{
 
+    private String currency;
+
     public Franc(int amount) {
         this.amount = amount;
+        currency = "CHF";
     }
 
     public String currency(){
-        return "CHF";
+        return currency;
     }
 
     public Money times(int multiplier){
