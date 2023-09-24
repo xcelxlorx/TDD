efe40022e5768a3a7cbd28cf5abc2961c9fac8c5
xcelxlorx <xcelxlorx@gmail.com>
2023-09-24 19:54:44 +0900

    feat: Money를 추상클래스로 변경한 후 Money에 times 메소드 선언하기

--- tdd/src/main/java/org/gihae/Money.java
+++ tdd/src/main/java/org/gihae/Money.java
@@ -1,15 +1,16 @@
 package org.gihae;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+    public abstract Money times(int multiplier);
+
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
 
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount && getClass().equals(money.getClass());
-    }
-
-    public static Dollar dollar(int amount){
-        return new Dollar(amount);
     }
 }
 
