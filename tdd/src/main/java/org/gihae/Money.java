b31060712719a5de44662503eb23d108495ce63a
xcelxlorx <xcelxlorx@gmail.com>
2023-09-24 20:03:44 +0900

    feat: Franc도 동일하게 구현해보자
    - 이제 times()를 정말 제거해야 함!

--- tdd/src/main/java/org/gihae/Money.java
+++ tdd/src/main/java/org/gihae/Money.java
@@ -8,6 +8,10 @@ public abstract class Money {
         return new Dollar(amount);
     }
 
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
+
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount && getClass().equals(money.getClass());
--- tdd/src/test/java/MultiplicationTest.java
+++ tdd/src/test/java/MultiplicationTest.java
@@ -64,14 +64,18 @@ class MultiplicationTest {
     void test8(){
         //test multiplication
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+
+        five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
 
         //test equality
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(new Franc(5).equals(new Franc(5)));
-        assertFalse(new Franc(5).equals(new Franc(6)));
-        assertFalse(new Franc(5).equals(Money.dollar(5)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 }
